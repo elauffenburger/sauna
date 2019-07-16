68df6e5f328a60eed2cfed34a3a26fa3cf121f5d
Eric Lauffenburger <elauffenburger@gmail.com>
2019-07-15 22:20:10 -0700

    add keyboard polling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 extern crate websocket;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 pub mod logger;
 
-use rppal::gpio::Gpio;
-use rppal::system::DeviceInfo;
 use std::thread;
+use std::io::{self, Write};
+use std::sync::mpsc::channel;
+use std::cell::RefCell;
+use std::rc::Rc;
 use websocket::sync::Server;
 use websocket::OwnedMessage;
 
 use logger::*;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SaunaMessage {
+    pub t: String
+}
 
 fn make_logger<'a>(name: &'a str) -> Box<Logger> {
     Box::new(ConsoleLogger::new(name.to_string()))
@@ -28,47 +39,36 @@ fn main() {
 
     let server = Server::bind(&server_addr).unwrap();
 
-    server_logger.info(format!(
-        "Welcome to sauna on {}!\n",
-        DeviceInfo::new().unwrap().model()
-    ));
-
+    server_logger.info(format!("Welcome to sauna!\n"));
     server_logger.info(format!("Server listening on {}", &server_addr));
 
     for request in server.filter_map(Result::ok) {
-        thread::spawn(|| {
+        thread::spawn(move || {
             let mut req_logger = make_logger("thread-logger");
-            let gpio = Gpio::new().unwrap();
 
             let mut conn = request.accept().unwrap();
             let ip = conn.peer_addr().unwrap();
 
             req_logger.debug(format!("Received connection from {}", ip));
 
-            let message = OwnedMessage::Text("Hi!".to_string());
-            conn.send_message(&message).unwrap();
-
             let (mut rx, mut tx) = conn.split().unwrap();
 
-            for msg in rx.incoming_messages() {
-                match msg {
-                    Err(err) => {
-                        req_logger.error(format!("Error: {}", err));
-                        break;
-                    }
-                    Ok(message) => match message {
-                        OwnedMessage::Text(msg_text) => {
-                            req_logger
-                                .debug(format!("Received message from {}: '{}'", ip, msg_text));
+            let ready_message = OwnedMessage::Text(serde_json::to_string(&SaunaMessage{ t: "ready".trim().to_string() }).unwrap());
+            tx.send_message(&ready_message).unwrap();
 
-                            tx.send_message(&OwnedMessage::Text(msg_text.to_string()));
-                        }
-                        raw_msg @ _ => {
-                            req_logger.debug(format!("Received non-text message: {:?}", raw_msg))
-                        }
-                    },
-                }
+            loop {
+                print!("{}:>", ip);
+                io::stdout().flush().unwrap();
+
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).unwrap();
+
+                tx.send_message(&format_message(SaunaMessage{ t: input.trim_right().to_string() }));
             }
         });
     }
+}
+
+fn format_message(msg: SaunaMessage) -> OwnedMessage {
+    OwnedMessage::Text(serde_json::to_string(&msg).unwrap())
 }
